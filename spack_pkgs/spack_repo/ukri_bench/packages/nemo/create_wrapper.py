4bb4261e71414c8910587e107d9c489b72c15f09
Garth N. Wells <gnw20@cam.ac.uk>
2025-07-01 07:43:36 +0100

    Add nemo

---
+++ spack_pkgs/spack_repo/ukri_bench/packages/nemo/create_wrapper.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import textwrap
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mpi', action='store_true', help="Enable MPI support")
+    parser.add_argument('--xios', action='store_true', help="Enable XIOS support")
+    parser.add_argument('--prefix', type=str, required=True, help="Prefix path")
+    args = parser.parse_args()
+    
+    wrapper_script = f"""
+    #!/bin/bash
+    
+    show_help() {{
+    cat << EOF
+      Usage: $(basename "$0") [OPTIONS]
+    
+      Options:
+      -d, --dir path        Run directory path (default: $PWD/NEMO_RUNDIR)
+    EOF
+      {'''echo "  -i, --lon num         (required) No. of NEMO MPI processes in the i (longitudinal) direction"
+      echo "  -j, --lat num         (required) No. of NEMO MPI processes in the j (latitudinal) direction"''' if args.mpi else ''}
+      {'echo "  -x, --xproc num       No. of XIOS servers (set to 0 if using attached mode) (default: 0)"' if args.xios else ''}
+    cat << EOF
+      -t, --timesteps num   No. of timesteps (default: 24)
+      -n, --namelist path   Namelist file path (default: nemo_path/EXP00/namelist_cfg)
+      --extra-paths paths   Extra paths to link in the run directory (colon separated)
+      +stats                Enable run.stat
+      +timings              Enable NEMO timings in the timing.output file (requires: gnuplot)
+      +icebergs             Enable icebergs
+      -h, --help            Show this help message and exit
+    EOF
+    }}
+    
+    REF_DIR="{args.prefix}/EXP00"
+    RUN_DIR="${{PWD}}/NEMO_RUNDIR"
+    XPROC=0
+    RUNLEN=24
+    NML=${{REF_DIR}}/namelist_cfg
+    TIMINGS=0
+    STATS=0
+    ICEBERGS=0
+    {'' if args.mpi else '''
+    IPROC=1
+    JPROC=1
+    '''}
+    
+    while [[ "$#" -gt 0 ]]; do
+      case "$1" in
+        -d|--dir)
+          RUN_DIR="$2"
+          shift 2
+          ;;
+        {'''-i|--lon)
+            IPROC="$2"
+            shift 2
+          ;;
+        -j|--lat)
+            JPROC="$2"
+            shift 2
+          ;;''' if args.mpi else ''}
+        {'''-x|--xproc)
+            XPROC="$2"
+            shift 2
+          ;;''' if args.xios else ''}
+        -t|--timesteps)
+          RUNLEN="$2"
+          shift 2
+          ;;
+        -n|--namelist)
+          NML="$2"
+          shift 2
+          ;;
+        --extra-paths)
+          EXTRA_PATHS="$2"
+          shift 2
+          ;;
+        +stats)
+          STATS=1
+          shift
+          ;;
+        +timings)
+          TIMINGS=1
+          shift
+          ;;
+        +icebergs)
+          ICEBERGS=1
+          shift
+          ;;
+        -h|--help)
+          show_help
+          exit 0
+          ;;
+        -*)
+          echo "Unknown option: $1"
+          show_help
+          exit 1
+          ;;
+        *)
+          echo "Unexpected argument: $1"
+          show_help
+          exit 1
+          ;;
+      esac
+    done
+    
+    {'''if [[ -z "$IPROC" ]]; then
+      echo "Error: -i is required."
+      show_help
+      exit 1
+    fi
+    
+    if [[ -z "$JPROC" ]]; then
+      echo "Error: -j is required."
+      show_help
+      exit 1
+    fi''' if args.mpi else ''}
+    
+    [ ! -f ${{NML}} ] && echo "No such namelist file ${{NML}}" && exit 1
+    
+    echo RUN DIR is $RUN_DIR
+    
+    [ -d ${{RUN_DIR}} ] && rm -r ${{RUN_DIR}}
+    cp -ansT ${{REF_DIR}} ${{RUN_DIR}}
+    cp --remove-destination ${{NML}} ${{RUN_DIR}}/namelist_cfg
+    {'cp --remove-destination ${REF_DIR}/iodef.xml ${RUN_DIR}/iodef.xml' if args.xios else ''}
+    
+    IFS=':' read -ra extra_paths <<< "$EXTRA_PATHS"
+    for p in "${{extra_paths[@]}}"; do
+      find ${{p}} -type f -exec ln -s {{}} ${{RUN_DIR}} \\;
+    done
+   
+    sed -r -i -e "s/(jpni[ ]*=[ ]*)([0-9]+)/\\1${{IPROC}}/" \\
+              -e "s/(jpnj[ ]*=[ ]*)([0-9]+)/\\1${{JPROC}}/" \\
+              -e "s/(nn_itend[ ]*=[ ]*)([0-9]+)/\\1${{RUNLEN}}/" ${{RUN_DIR}}/namelist_cfg
+    
+    if [[ ${{TIMING}} -eq 1 ]]; then
+      f90nml -g namctl -v ln_timing=true ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    else
+      f90nml -g namctl -v ln_timing=false ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    fi
+    
+    if [[ ${{STATS}} -eq 1 ]]; then
+      f90nml -g namctl -v sn_cfctl%l_runstat=true ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    else
+      f90nml -g namctl -v sn_cfctl%l_runstat=false ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    fi
+
+    if [[ ${{ICEBERGS}} -eq 1 ]]; then
+      f90nml -g namberg -v ln_icebergs=true ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    else
+      f90nml -g namberg -v ln_icebergs=false ${{RUN_DIR}}/namelist_cfg ${{RUN_DIR}}/namelist_cfg
+    fi
+    """
+    
+    wrapper_path = os.path.join(args.prefix, "bin")
+    os.makedirs(wrapper_path, exist_ok=True)
+    with open(os.path.join(wrapper_path, "nemo-wrapper"), "w") as f:
+        f.write(textwrap.dedent(wrapper_script))
+    os.chmod(os.path.join(wrapper_path, "nemo-wrapper"), 0o755)
+
+if __name__ == "__main__":
+    main()
---
+++ spack_pkgs/spack_repo/ukri_bench/packages/nemo/package.py
@@ -0,0 +1,332 @@
+import os
+import sys
+import textwrap
+from pathlib import Path
+from spack.package import *
+
+
+class Nemo(Package):
+    """
+    The 'Nucleus for European Modelling of the Ocean' (NEMO) is a
+    state-of-the-art modelling framework. It is used for research
+    activities and forecasting services in ocean and climate sciences.
+    """
+
+    homepage = "https://www.nemo-ocean.eu/"
+    git = "https://forge.nemo-ocean.eu/nemo/nemo.git"
+    version("5.0", commit="bc25452d80459627cb1d1ee9e6a128d7932d6e1b")
+    maintainers("addy419")
+
+    phases = ("configure", "build", "install")
+
+    # --- Representative configs ---
+    nmconfigs = {
+        "ORCA2_ICE_PISCES": {"ice": True, "pisces": True},
+        "BENCH": {"ice": True, "pisces": True},
+        "GOSI10p0.0_like_eORCA1": {"ice": True, "pisces": False},
+        "GOSI10p0.0_like_eORCA025": {"ice": True, "pisces": False},
+        "GOSI10p0.0_like_eORCA12": {"ice": True, "pisces": False},
+        "GENERIC": {"ice": True, "pisces": True},
+    }
+
+    # --- Variants ---
+    variant("mpi", default=True, description="Enable MPI support")
+    variant(
+        "ice",
+        default=False,
+        description="""Enable SEA-ICE
+        In polar regions temperatures become cold enough for seawater to freeze and sea ice forms on the surface of the ocean. NEMO includes a sea-ice model component, known as SI³ (Sea Ice modelling Integrated Initiative), which runs along with the ocean component with a different set of equations.""",
+    )
+    variant(
+        "pisces",
+        default=False,
+        description="""Enable PISCES
+        PISCES is a biogeochemical model that simulates marine biological productivity and describes the biogeochemical cycles of carbon, oxygen and of the main nutrients (P, N, Si, Fe) (Aumont et al., 2015).""",
+    )
+    variant("xios", default=False, description="Enable XIOS IO server support")
+    variant(
+        "openmp",
+        default=False,
+        description="Apply OpenMP transforms to NEMO through PSyclone",
+    )
+    # TODO: Add GPU support
+    # variant('openmp-offload', default=False, description='Apply OpenMP GPU transforms to NEMO through PSyclone')
+    variant(
+        "config",
+        default="ORCA2_ICE_PISCES",
+        values=(tuple(nmconfigs.keys())),
+        multi=False,
+        description="Build the specified NEMO source configuration",
+    )
+    variant(
+        "generic_config",
+        default="none",
+        values=str,
+        description="Specify the generic configuration to use",
+    )
+
+    # --- Conflicts ---
+    conflicts(
+        "+xios", msg="The XIOS spack package is currently broken and cannot be used"
+    )
+    conflicts(
+        "+openmp",
+        msg="The py-psyclone package is currently broken with spackv0.23.1, change if newer versions work",
+    )
+    conflicts(
+        "config=GENERIC",
+        when="generic_config=none",
+        msg="generic_config should be set when using config=GENERIC",
+    )
+
+    for key, value in nmconfigs.items():
+        if value["ice"] == False:
+            conflicts(
+                "+ice",
+                when=f"config={key}",
+                msg=f"{key} does not support SEA-ICE. Use config=GENERIC to circumvent this",
+            )
+        if value["pisces"] == False:
+            conflicts(
+                "+pisces",
+                when=f"config={key}",
+                msg=f"{key} does not support PISCES. Use config=GENERIC to circumvent this",
+            )
+
+    # --- Compilers ---
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
+
+    # --- Dependencies ---
+    depends_on("binutils", type="build")
+    depends_on("gmake", type="build")
+    # nemo segfaults without mpi even if we aren't using it
+    depends_on("mpi", type="build")
+    depends_on("hdf5 +shared +fortran +mpi", type="build")
+    depends_on("xios@2.5:", type=("build", "link"), when="+xios")
+    depends_on("netcdf-c@4.9.0: +mpi +shared", type=("build", "link"))
+    depends_on("netcdf-fortran@4.6.1: +shared", type="build")
+    depends_on("py-f90nml", type="run")
+    depends_on("py-psyclone", type="build", when="+openmp")
+
+    # --- Patches ---
+    patch("makenemo.patch")
+    patch("sct_psyclone.patch")
+
+    # --- Variables ---
+    config_name = "BLDCFG"
+    source_cfg = None
+    source_path = None
+    config_path = None
+    add_keys = []
+    del_keys = []
+
+    @run_before("build")
+    def set_config_paths(self):
+        nemo_root = self.stage.source_path
+        self.source_cfg = self.spec.variants["config"].value
+        if self.source_cfg == "GENERIC":
+            self.source_cfg = self.spec.variants["generic_config"].value
+
+        cfgs = Path(join_path(nemo_root, "cfgs", self.source_cfg))
+        tests = Path(join_path(nemo_root, "tests", self.source_cfg))
+
+        if cfgs.is_dir():
+            self.source_path = cfgs
+        elif tests.is_dir():
+            self.source_path = tests
+        else:
+            raise InstallError(
+                f"Configuration failed: Cannot find source configuration {self.source_cfg}"
+            )
+
+        self.config_path = Path(join_path(self.source_path.parent, self.config_name))
+
+    def setup_build_environment(self, env):
+        if self.spec.satisfies("+xios"):
+            env.set("XIOS_PATH", self.spec["xios"].prefix)
+        if self.spec.satisfies("+openmp"):
+            utilspath = join_path(
+                self.spec["py-psyclone"].prefix.share,
+                "psyclone",
+                "examples",
+                "nemo",
+                "scripts",
+            )
+            env.prepend_path("PYTHONPATH", utilspath)
+
+    def configure(self, spec, prefix):
+        ar = join_path(spec["binutils"].prefix.bin, "ar")
+        gmake = join_path(spec["gmake"].prefix.bin, "gmake")
+        fcompiler = spec["mpi"].mpifc
+        h5dir = spec["hdf5"].prefix
+        ncdir = spec["netcdf-c"].prefix
+        nfdir = spec["netcdf-fortran"].prefix
+        xiosdir = ""
+        psydir = ""
+        arch_extra = ""
+
+        if spec.satisfies("+xios"):
+            xiosdir = str(spec["xios"].prefix)
+
+        if spec.satisfies("+openmp"):
+            psydir = str(spec["py-psyclone"].prefix)
+
+        # TODO: Add -march, check how spack does it
+
+        if spec.satisfies("%gcc"):
+            fflags = "-fdefault-real-8 -O2 -funroll-all-loops -fcray-pointer -ffree-line-length-none"
+            ldflags = "-fdefault-real-8"
+            if spec.satisfies("+openmp"):
+                fflags += " -fopenmp"
+                ldflags += " -fopenmp"
+        elif spec.satisfies("%nvhpc"):
+            fflags = "-i4 -Mr8 -Mnovect -Mflushz -Minline -Mnofma -O2 -gopt -traceback"
+            ldflags = "-i4 -Mr8 -Mnofma"
+            if spec.satisfies("+openmp"):
+                fflags += " -mp"
+                ldflags += " -mp"
+        elif spec.satisfies("%oneapi"):
+            fflags = "-i4 -r8 -O2 -fp-model strict -xHost -fno-alias"
+            ldflags = "-i4 -r8"
+            if spec.satisfies("+openmp"):
+                fflags += " -fiopenmp"
+                ldflags += " -fiopenmp"
+        elif spec.satisfies("%cce"):
+            fflags = "-em -s integer32 -s real64 -O2 -hvector_classic -hflex_mp=intolerant -N1023 -M878"
+            ldflags = ""
+            arch_extra = "bld::tool::fc_modsearch -J"
+            if spec.satisfies("+openmp"):
+                fflags += " -h omp"
+                ldflags += " -h omp"
+
+        arch = textwrap.dedent(
+            f"""
+            %NCDFF_HOME          {nfdir}
+            %NCDFC_HOME          {ncdir}
+            %HDF5_HOME           {h5dir}
+            %XIOS_HOME           {xiosdir}
+            %PSYCLONE_HOME       {psydir}
+            
+            %NCDF_INC            -I%NCDFF_HOME/include
+            %NCDF_LIB            -L%NCDFF_HOME/lib -lnetcdff -L%NCDFC_HOME/lib -lnetcdf -L%HDF5_HOME/lib -lhdf5_hl -lhdf5 -lhdf5
+            %XIOS_INC            -I%XIOS_HOME/inc
+            %XIOS_LIB            -L%XIOS_HOME/lib -lxios -lstdc++
+            %CPP	             cpp -Dkey_nosignedzero
+            %FC                  {fcompiler}
+            %PROD_FCFLAGS        {fflags}
+            %DEBUG_FCFLAGS       
+            %FFLAGS
+            %LD                  {fcompiler}
+            %LDFLAGS             {ldflags} -Wl,-rpath=%HDF5_HOME/lib -Wl,-rpath=%NCDFF_HOME/lib -Wl,-rpath=%XIOS_HOME/lib
+            %FPPFLAGS            -P -traditional
+            %AR                  {ar}
+            %ARFLAGS             -rs
+            %MK                  {gmake}
+            %USER_INC            %XIOS_INC %NCDF_INC
+            %USER_LIB            %XIOS_LIB %NCDF_LIB
+            {arch_extra}
+        """
+        )
+
+        arch_fcm = join_path(self.stage.source_path, "arch", "arch-fort.fcm")
+        with open(arch_fcm, "w") as f:
+            f.write(arch)
+
+        if spec.satisfies("+xios"):
+            self.add_keys.append("key_iomput")
+            if spec["xios"].version.satisfies("@3.0:"):
+                self.del_keys.append("key_xios")
+                self.add_keys.append("key_xios3")
+            elif spec["xios"].version.satisfies("@2.5:"):
+                self.del_keys.append("key_xios3")
+                self.add_keys.append("key_xios")
+        else:
+            self.del_keys.append("key_xios")
+            self.del_keys.append("key_xios3")
+            self.del_keys.append("key_iomput")
+
+        if spec.satisfies("+mpi"):
+            if spec.satisfies("@4.2:"):
+                self.del_keys.append("key_mpi_off")
+            elif spec.satisfies("@=4.0:"):
+                self.add_keys.append("key_mpp_mpi")
+        else:
+            if spec.satisfies("@4.2:"):
+                self.add_keys.append("key_mpi_off")
+            elif spec.satisfies("@=4.0:"):
+                self.del_keys.append("key_mpp_mpi")
+
+        # TODO:
+        # if spec.satisfies("%nvhpc"):
+        #    self.add_keys.append("key_nosignedzero")
+
+        if spec.satisfies("+ice"):
+            self.add_keys.append("key_si3")
+        else:
+            self.del_keys.append("key_si3")
+
+        if spec.satisfies("+pisces"):
+            self.add_keys.append("key_top")
+        else:
+            self.del_keys.append("key_top")
+
+    def build(self, spec, prefix):
+        params = []
+
+        params.append("-j")
+        params.append(f"{make_jobs}")
+        params.append("-m")
+        params.append(f"fort")
+
+        if self.spec.satisfies("+openmp"):
+            trans = join_path(
+                self.spec["py-psyclone"].prefix.share,
+                "psyclone",
+                "examples",
+                "nemo",
+                "scripts",
+                "omp_cpu_trans.py",
+            )
+            params.append("-p")
+            params.append(trans)
+
+        if self.config_path.parent.name == "cfgs":
+            params.append("-r")
+        elif self.config_path.parent.name == "tests":
+            params.append("-a")
+        params.append(self.source_cfg)
+        params.append("-n")
+        params.append(self.config_name)
+
+        if self.del_keys:
+            params.append("del_key")
+            params.append(" ".join(self.del_keys))
+        if self.add_keys:
+            params.append("add_key")
+            params.append(" ".join(self.add_keys))
+
+        with working_dir(self.stage.source_path):
+            makenemo = Executable(join_path(".", "makenemo"))
+            makenemo(*params)
+
+        if spec.satisfies("+xios"):
+            xios_exe = join_path(self.spec["xios"].prefix.bin, "xios_server.exe")
+            xios_run_exe = join_path(self.config_path, "EXP00", "xios_server.exe")
+            if not os.path.exists(xios_run_exe):
+                os.symlink(xios_exe, xios_run_exe)
+
+    def install(self, spec, prefix):
+        params = []
+        params.append(join_path(self.package_dir, "create_wrapper.py"))
+        params.append("--prefix=" + prefix)
+
+        if spec.satisfies("+xios"):
+            params.append("--xios")
+        if spec.satisfies("+mpi"):
+            params.append("--mpi")
+
+        python = Executable("python3")
+        python(*params)
+
+        install_tree(str(self.config_path), prefix, symlinks=False)

51bf1549474365000ee2dde9961f1a2088a2b8e8
Garth N. Wells <gnw20@cam.ac.uk>
2025-12-22 19:25:49 +0000

    Test boost issue

--- spack_pkgs/spack_repo/ukri_bench/packages/bench_dolfinx/package.py
+++ spack_pkgs/spack_repo/ukri_bench/packages/bench_dolfinx/package.py
@@ -24,7 +24,7 @@ class BenchDolfinx(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("fenics-basix")
     depends_on("py-fenics-ffcx", type="build")
 
-    depends_on("boost@:1.86 +program_options")  # See https://github.com/boostorg/math/issues/1285
+    depends_on("boost +program_options")  # See https://github.com/boostorg/math/issues/1285
     depends_on("jsoncpp")
     depends_on("mpi")
 
